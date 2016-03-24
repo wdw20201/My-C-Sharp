9d9a70092f8866c1ff60a8f8224163adc83a6182
Wiiliam Wood <wdw2050@gmail.com>
2016-03-24 16:32:41 -0700

    first commit

---
+++ Tickets/Tickets/FrmTickets.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Tickets
+{
+    partial class FrmTickets
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTickets = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnTickets
+            // 
+            this.BtnTickets.Location = new System.Drawing.Point(53, 95);
+            this.BtnTickets.Name = "BtnTickets";
+            this.BtnTickets.Size = new System.Drawing.Size(175, 23);
+            this.BtnTickets.TabIndex = 0;
+            this.BtnTickets.Text = "Determine Tickets Price";
+            this.BtnTickets.UseVisualStyleBackColor = true;
+            this.BtnTickets.Click += new System.EventHandler(this.BtnTickets_Click);
+            // 
+            // FrmTickets
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BtnTickets);
+            this.Name = "FrmTickets";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tickets";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTickets;
+    }
+}
+
---
+++ Tickets/Tickets/FrmTickets.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tickets
+{
+    public partial class FrmTickets : Form
+    {
+        public FrmTickets()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTickets_Click(object sender, EventArgs e)
+        {
+            int age;
+            double price=0;
+
+            age = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Please enter Your Age ", "Tickets", ""));
+
+            if (age <=12)
+            {
+                price = 5.00;
+            }
+            else if (age >=13 && age <=20)
+            {
+                price = 6.00;
+
+            }
+            else if (age >=21 && age <= 64)
+            {
+                price = 10.00;
+            }
+            else if (age >= 65)
+            {
+                price = 2.00;
+            }
+            MessageBox.Show("Your price is" + price.ToString("c"));
+        }
+    }
+}
